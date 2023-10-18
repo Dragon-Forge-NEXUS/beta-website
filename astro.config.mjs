b0218efdff3f18fed1a6d577397f6042d5f1bf59
Altie <altielynn@gmail.com>
2023-10-17 21:32:49 -0600

    v0.1.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwind from '@astrojs/tailwind';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://beta.altiesgamingstudios.com',
-  integrations: [sitemap(), tailwind()]
+  site: 'https://nightly.dragonforgenexus.xyz',
+  integrations: [sitemap(), tailwind(), mdx()]
 });
--- src/pages/Partners/requirements.md
+++ src/pages/Partners/requirements.md
@@ -6,3 +6,5 @@
 ## Twitch FYTS:
 * 50+ followers
 * Stream atleast once a month
+
+#### WE HARDLY EVER IMFORCE THE REQUIREMENTS
